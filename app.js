68ebe15570a56164e35dc52d92ba3390b44b3bf2
Rae Chin <ruihanchin@gmail.com>
2022-06-19 21:29:36 +0800

    404 page

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -12,7 +13,7 @@ app.use('/admin', adminRoutes)
 app.use(resultRoutes)
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>404 Page not found!</h1>')
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 });
 
 app.listen(8080);
