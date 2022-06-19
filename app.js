9012a3093823f4d007b54fa1d25ceb75d26fe8f6
Rae Chin <ruihanchin@gmail.com>
2022-06-19 19:50:31 +0800

    bodyParser

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
-const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const routes = require('./routes');
+const app = express();
 
-console.log(routes.someText);
+app.use(bodyParser.urlencoded({extended: false}));
 
-const server = http.createServer(routes.handler);
+app.use('/add-product', (req, res, next) => {
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+});
 
-server.listen(8080);
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
+  res.send('<h1>Hello from Express!</h1>');
+});
+
+app.listen(8080);
