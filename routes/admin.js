2c5bd59afa668c10d559d4fce8276df0ea9687a7
Rae Chin <ruihanchin@gmail.com>
2022-06-19 22:27:11 +0800

    adding css

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,12 @@
 const path = require('path');
 const express = require('express');
 
+const rootDir = require('../util/path')
+
 const router = express.Router();
 
 router.get('/input-details', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '../', 'views', 'input-details.html'));
+  res.sendFile(path.join(rootDir, 'views', 'input-details.html'));
 });
 
 router.post('/benefits', (req, res, next) => {
--- routes/result.js
+++ routes/result.js
@@ -2,10 +2,12 @@ const path = require('path')
 
 const express = require('express');
 
+const rootDir = require('../util/path')
+
 const router = express.Router()
 
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '../', 'views', 'home.html'));
+    res.sendFile(path.join(rootDir, 'views', 'home.html'));
   });
 
 module.exports = router
